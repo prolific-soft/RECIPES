49179f376fdcf4c3a1c7534ecc8d28a047d060ec
Chidi Emeh <chidiemeh184@gmail.com>
2017-12-26 02:20:02 -0500

    Slide UITextField up on keyboard appear SignUpVC

--- RECIPES/SignUpViewController.swift
+++ RECIPES/SignUpViewController.swift
@@ -8,8 +8,18 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UITextFieldDelegate {
 
+    //Class Properties
+    
+    @IBOutlet weak var niceToSeeYouLabel: UILabel!
+    @IBOutlet weak var signUpLabel: CustomLabel!
+    @IBOutlet weak var usernameTextField: CustomTextField!
+    @IBOutlet weak var emailTextField: CustomTextField!
+    @IBOutlet weak var passwordTextField: CustomTextField!
+    
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpViewProperties()
@@ -17,7 +27,7 @@ class SignUpViewController: UIViewController {
 
     //Sets up the view properties
     func setUpViewProperties() {
-        self.navigationController?.navigationBar.frame.origin.y = 20
+        //self.navigationController?.navigationBar.frame.origin.y = 20
     }
     
     //Pops the current VC on stack
@@ -26,9 +36,39 @@ class SignUpViewController: UIViewController {
         
     }
     
-    //Hides status bar
-    override var prefersStatusBarHidden: Bool{
-        return true
-    }
+//    //Hides status bar
+//    override var prefersStatusBarHidden: Bool{
+//        return true
+//    }
 
 }
+
+
+//Slide Up Keyboard
+extension SignUpViewController {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.navigationController?.navigationBar.isHidden = true
+        self.signUpLabel.isHidden = true
+        self.niceToSeeYouLabel.isHidden = true
+        self.topConstraint.constant = CGFloat(40)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        topConstraint.constant = CGFloat(80)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.navigationController?.navigationBar.isHidden = false
+        self.signUpLabel.isHidden = false
+        self.niceToSeeYouLabel.isHidden = false
+        self.topConstraint.constant = CGFloat(80)
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+
+
+
+
+
