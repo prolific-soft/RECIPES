9b912dd12d87df2f718ba6f06fc892697a9578e0
Chidi Emeh <chidiemeh184@gmail.com>
2017-12-27 12:11:16 -0500

    Fixed Constraint for HomeTableView cell : (StackView All The things! ;-))

---
+++ RECIPES/Extension.swift
@@ -0,0 +1,101 @@
+//
+//  Extension.swift
+//  RECIPES
+//
+//  Created by Chidi Emeh on 12/26/17.
+//  Copyright © 2017 Chilly Bean. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+////== UIVIEW - Layer/Boarder Properties ==//
+//@IBDesignable
+//extension UIView {
+//    
+//    @IBInspectable
+//    var cornerRadius: CGFloat {
+//        get {
+//            return layer.cornerRadius
+//        }
+//        set {
+//            layer.cornerRadius = newValue
+//        }
+//    }
+//    
+//    @IBInspectable
+//    var borderWidth: CGFloat {
+//        get {
+//            return layer.borderWidth
+//        }
+//        set {
+//            layer.borderWidth = newValue
+//        }
+//    }
+//    
+//    @IBInspectable
+//    var borderColor: UIColor? {
+//        get {
+//            if let color = layer.borderColor {
+//                return UIColor(cgColor: color)
+//            }
+//            return nil
+//        }
+//        set {
+//            if let color = newValue {
+//                layer.borderColor = color.cgColor
+//            } else {
+//                layer.borderColor = nil
+//            }
+//        }
+//    }
+//    
+//    @IBInspectable
+//    var shadowRadius: CGFloat {
+//        get {
+//            return layer.shadowRadius
+//        }
+//        set {
+//            layer.shadowRadius = newValue
+//        }
+//    }
+//    
+//    @IBInspectable
+//    var shadowOpacity: Float {
+//        get {
+//            return layer.shadowOpacity
+//        }
+//        set {
+//            layer.shadowOpacity = newValue
+//        }
+//    }
+//    
+//    @IBInspectable
+//    var shadowOffset: CGSize {
+//        get {
+//            return layer.shadowOffset
+//        }
+//        set {
+//            layer.shadowOffset = newValue
+//        }
+//    }
+//    
+//    @IBInspectable
+//    var shadowColor: UIColor? {
+//        get {
+//            if let color = layer.shadowColor {
+//                return UIColor(cgColor: color)
+//            }
+//            return nil
+//        }
+//        set {
+//            if let color = newValue {
+//                layer.shadowColor = color.cgColor
+//            } else {
+//                layer.shadowColor = nil
+//            }
+//        }
+//    }
+//}
+
