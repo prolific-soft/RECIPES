19f0d76c68424d63734b5a1d4b70ef3216397dd6
Chidi Emeh <chidiemeh184@gmail.com>
2017-12-27 11:45:47 -0500

    Fixed and resized all icons

--- RECIPES/NetworkProcessor.swift
+++ RECIPES/NetworkProcessor.swift
@@ -14,4 +14,93 @@ class NetworkProcessor {
     lazy var configuraiton = URLSessionConfiguration.default
     lazy var session = URLSession(configuration: configuraiton)
     
+    var url : URL?
+    
+    init(url : URL) {
+        self.url = url
+    }
+    
+    typealias JSONDownloader = ((Codable?)-> Void)
+    typealias IMAGEDATADownloader = ((Data?, HTTPURLResponse?, Error?)-> Void)
+    
+    
+    //Download Recipe Data
+    func downloadRecipe(withStructType : String, _ completion : @escaping JSONDownloader){
+        
+        guard let downloadURL = self.url else { return }
+        let request = URLRequest(url: downloadURL)
+        
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            if error != nil {
+                if let dataResponse = response as? HTTPURLResponse {
+                    
+                    switch dataResponse.statusCode {
+                    case 200:
+                        //Status Ok
+                        guard let downloadData = data else { return }
+                        
+                            do {
+                                switch withStructType {
+                                case "recipe":
+                                    let recipeJSON = try JSONDecoder().decode(Recipe.self, from: downloadData)
+                                    completion(recipeJSON)
+                                case "recipes":
+                                    let recipesJSON = try JSONDecoder().decode(Recipes.self, from: downloadData)
+                                    completion(recipesJSON)
+                                default:
+                                    print("No form of recipe")
+                                }
+                            }catch let decodingError as NSError{
+                                print("Error Decoding : \(decodingError)")
+                            }
+                        print("Status OK")
+                    default:
+                        print("Recipe Reesponse Error Status code : \(dataResponse.statusCode)")
+                    }
+                }
+            }else {
+                print("Error downloading \(error.debugDescription)")
+            }
+        }
+        
+        dataTask.resume()
+    }
+    
+    
+    //Download Image Data
+    func downloadImage(_ completion : @escaping IMAGEDATADownloader ){
+        guard let imageUrl = self.url else { return }
+        let imageRequest = URLRequest(url: imageUrl)
+        
+        let imageDataTask = session.dataTask(with: imageRequest) { (imageData, imageResponse, imageError) in
+           
+            if imageError == nil {
+                guard let response = imageResponse as? HTTPURLResponse else {return}
+                
+                switch response.statusCode {
+                case 200:
+                    guard let data = imageData else {return}
+                    completion(data, nil, nil )
+                default:
+                    return
+                }
+            }else {
+                print("Error downloading Image:  \(imageError.debugDescription)")
+                completion(nil, nil, imageError!)
+            }
+        }
+        imageDataTask.resume()
+    }
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
