d555f2cfc014d26ec8cff24ae67f768f4bcc061a
Chidi Emeh <chidiemeh184@gmail.com>
2017-12-29 13:38:03 -0500

    Ingredient and Instruction Header

---
+++ RECIPES/RecipeOpenTableHeaderView.swift
@@ -0,0 +1,26 @@
+//
+//  RecipeOpenTableHeaderView.swift
+//  RECIPES
+//
+//  Created by Chidi Emeh on 12/29/17.
+//  Copyright © 2017 Chilly Bean. All rights reserved.
+//
+
+import UIKit
+
+class RecipeOpenTableHeaderView: UIView {
+
+  //UI Properties
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var backgroundView: UIView!
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
